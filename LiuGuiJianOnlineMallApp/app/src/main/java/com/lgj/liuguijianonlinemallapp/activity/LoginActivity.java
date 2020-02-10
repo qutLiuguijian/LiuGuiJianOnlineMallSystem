f4461e708b9202baa04fd75aa3f978ca588306a3
Administrator <13375371576@163.com>
2020-02-10 18:12:47 +0800

    test

---
+++ LiuGuiJianOnlineMallApp/app/src/main/java/com/lgj/liuguijianonlinemallapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.lgj.liuguijianonlinemallapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lgj.liuguijianonlinemallapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_login);
+    }
+}
