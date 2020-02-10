874bb2dcba0bbd14e73ec34be1eeec64626376d8
Administrator <13375371576@163.com>
2020-02-10 17:57:25 +0800

    test

---
+++ LiuGuiJianOnlineMallApp/app/src/main/java/com/lgj/liuguijianonlinemallapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lgj.liuguijianonlinemallapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lgj.liuguijianonlinemallapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
